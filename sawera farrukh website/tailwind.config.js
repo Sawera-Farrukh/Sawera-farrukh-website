99885780ad724901128c004c69caf7f2b187158b
Sawera-Farrukh <151993890+Sawera-Farrukh@users.noreply.github.com>
2026-07-15 22:42:53 +0500

    Add custom screen size 'desk' to Tailwind config
    Added custom screen size 'desk' at 1440px to Tailwind config.

--- sawera farrukh website/tailwind.config.js
+++ sawera farrukh website/tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   darkMode: ["class"],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      'desk': '1440px',
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
